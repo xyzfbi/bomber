0ad19df4bc5a00e298437326d2f8b179c58b3876
Першин Максим Дмитриевич <mdpershin@edu.hse.ru>
2025-02-22 00:48:27 +0300

    spammer by layk1i. any messenger u want. (trying to fix errorrs with bufer)

--- script.py
+++ script.py
@@ -1,8 +1,8 @@
 from tkinter import simpledialog
 import tkinter as tk
 import time
+import keyboard
 import pyperclip
-from pynput.keyboard import Controller
 
 root = tk.Tk()
 root.withdraw()
@@ -10,21 +10,15 @@ root.withdraw()
 x = simpledialog.askstring("Input", "Enter text to spam:")
 f = int(simpledialog.askstring("Input", "Enter number of repetitions:"))
 
-keyboard = Controller()
-
 def spam(text: str, amount: int):
     pyperclip.copy(text)
     time.sleep(1)
     for _ in range(amount):
-        time.sleep(0.2)
-        keyboard.press('v')
-        keyboard.release('v')
-        keyboard.press('\n')
-        keyboard.release('\n')
+        time.sleep(0.1)
+        keyboard.press_and_release("ctrl+v")
+        keyboard.press_and_release("enter")
 
-spam(x, f)
+spam(x  , f)
 
 print("Spamming completed!")
-
-keyboard._controller.stop()
-root.destroy()vv
+root.destroy()
